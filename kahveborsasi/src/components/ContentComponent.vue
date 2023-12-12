b45c695e08c1b9c959a2298604acea2e0b256c59
nedimgorgu <69355961+hilalpolatt07@users.noreply.github.com>
2023-12-12 19:20:48 +0300

    NavbarBottom projeye eklendi

---
+++ kahveborsasi/src/components/ContentComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col">
+
+        <NavbarBottom />
+        
+
+
+
+
+    </div>
+</template>
+
+<script lang="ts">
+
+import NavbarBottom from "../components/NavbarBottom.vue"
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+    components: { NavbarBottom }
+})
+</script>
