40563de98d077876b62e149ba74f3ef4049f1210
nedimgorgu <69355961+hilalpolatt07@users.noreply.github.com>
2023-12-12 19:58:00 +0300

    Carousel projeye eklendi

--- kahveborsasi/src/components/ContentComponent.vue
+++ kahveborsasi/src/components/ContentComponent.vue
@@ -2,7 +2,28 @@
     <div class="col">
 
         <NavbarBottom />
-        
+
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8 ">
+                <h4 class="text-center text-weight-bold">Taksitli ve Toptan Kahve</h4>
+                <div class="row">
+                    <div class="col-3">
+                        <CategoryList />
+                    </div>
+                    <div class="col-8 q-pl-lg">
+                        <CarouselComponent />
+                        
+                    </div>
+                </div>
+
+                
+
+                
+
+            </div>
+            <div class="col-2"></div>
+        </div>
 
 
 
@@ -13,11 +34,18 @@
 <script lang="ts">
 
 import NavbarBottom from "../components/NavbarBottom.vue"
-
-import { defineComponent } from 'vue';
+import CarouselComponent from "../components/CarouselComponent.vue"
+import { defineComponent, ref } from 'vue';
+import CategoryList from "./CategoryList.vue";
 
 export default defineComponent({
 
-    components: { NavbarBottom }
+    setup() {
+        return {
+            slide: ref(1)
+        }
+    },
+
+    components: { NavbarBottom, CategoryList, CarouselComponent }
 })
 </script>
